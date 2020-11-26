31c52b8e69ae037eabdde1a2a64f9a3e8c8dd618
Rivvnik <jacob@datacea.com>
2020-11-26 01:59:48 -0500

    added distinct token file to allow discrete utils.json

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
+import discord
 from discord.ext import commands
 import asyncpg
 import os
 import json
 from utils.embuilder import generator
 
-bot = commands.Bot(command_prefix='>', case_insensitive=True, owner_id=310863530591256577)
-bot.help_data = {}
+intents = discord.Intents.default()
+intents.members = True
+intents.reactions = True
+intents.presences = True
+intents.typing = True
+bot = commands.Bot(command_prefix='>', intents=intents, case_insensitive=True, owner_id=310863530591256577)
 bot.remove_command('help')
-bot.path = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'utils/utils.json')
-with open(bot.path, 'r') as f:
-    bot.utils = json.load(f)
+
+bot.token_path, bot.utils_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'utils/token.json'), os.path.join(os.path.split(os.path.abspath(__file__))[0], 'utils/utils.json')
+with open(bot.token_path, 'r') as x: bot.token = json.load(x)
+with open(bot.utils_path, 'r') as f: bot.utils = json.load(f)
+bot.utils["help_data"] = {}
+with open(bot.utils_path, 'w') as f: json.dump(bot.utils, f)
+bot.help_data = bot.utils["help_data"]
+
 extensions = (
     'cogs.IT',
     'cogs.utility'
@@ -32,6 +42,7 @@ async def help(ctx):
         await message.add_reaction(emoji)
     bot.help_data[message.id] = {}
     bot.help_data[message.id]["embed_list"], bot.help_data[message.id]["index"], bot.help_data[message.id]["author_id"] = embeds, 0, ctx.author.id
+    with open(bot.utils_path, 'w') as f: json.dump(bot.utils, f)
 
 if __name__ == '__main__':
     for extension in extensions:
@@ -41,4 +52,4 @@ if __name__ == '__main__':
             print(f'`{extension}` cannot be loaded. [{error}]')
 
 bot.loop.run_until_complete(create_db_pool())
-bot.run(bot.utils["TOKEN"])
+bot.run(bot.token["TOKEN"])
